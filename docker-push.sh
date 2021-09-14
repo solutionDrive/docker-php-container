e847adc31618e4f6205d2b8d8dea79e33247b3dc
Tobias Lückel <tobias.lueckel@netlogix.de>
2021-09-14 10:14:26 +0200

    Added push at the end of the workflow

--- docker-push.sh
+++ docker-push.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "${{ secrets.DOCKER_PASSWORD }}" | docker login -u "${{ secrets.DOCKER_USERNAME }}" --password-stdin
 docker push solutiondrive/docker-php-container
 docker push solutiondrive/php
